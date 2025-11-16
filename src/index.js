5f4d5b06e77972b87db87b265ea59dfed3467249
xinsanitiix <xinsanitiix@gmail.com>
2025-11-16 21:51:17 +0100

    weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,41 @@ function updateWeatherDetails(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = " " + response.data.condition.description;
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity + "%";
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed) + "mph";
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = `${formatDate(date)}`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let hours = date.getHours().toString().padStart(2, "0");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
